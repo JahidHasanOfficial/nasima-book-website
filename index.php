6fbdbb718932d24b2fb8d52310da610305b2caa8
Jahid Hasan <jahidhasanofficial23@gmail.com>
2026-02-23 23:05:36 +0600

    project

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  
+  <title>Home - Nasima Anwara</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  
+  <?php include('head_link.php'); ?>
+
+</head>
+
+<body class="index-page">
+
+  <?php include('header_nav.php'); ?>
+ 
+  <main class="main">
+  
+  <?php include('hero.php'); ?>
+  
+  <?php include('about_home.php'); ?>
+  
+  <?php // include('skill.php'); ?>
+  
+  <?php // include('resume.php'); ?>
+  
+  <?php // include('portfolio.php'); ?>
+  
+  <?php include('books_home.php'); ?>
+  
+  <?php include('testimonials.php'); ?>
+  
+  <?php // include('services.php'); ?>
+  
+  <?php include('faq.php'); ?>
+   
+ 
+  </main>
+  
+  
+  <?php include('footer.php'); ?>
+
+ 
